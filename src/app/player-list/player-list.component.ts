cece1e0ac232a7b5b96bfe31ce7286e6ca4e92a7
Otto Hu <ottohu101@gmail.com>
2018-07-11 23:38:57 -0700

    start working on playerList

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GameService } from '../../services/game.service';
+import { LanguageService } from '../../services/language.service';
+import { MatSelectionList, MatSelectionListChange } from '@angular/material';
 
 @Component({
   selector: 'app-player-list',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-list.component.scss']
 })
 export class PlayerListComponent implements OnInit {
+  public playerData = [];
+  @ViewChild(MatSelectionList) players: MatSelectionList;
 
-  constructor() { }
+  constructor(
+    private gs: GameService,
+    public ls: LanguageService
+  ) {
+    const roleObj = gs.getRoleData();
+    for (let i in roleObj) {
+      if(i == "werewolves"){
+        roleObj[i].forEach(werewolf => {
+          this.playerData.push({
+            role: "werewolf",
+            playerName: werewolf.name
+          });    
+        });
+      }
+      else if(i == "villagers"){
+        roleObj[i].forEach(villager => {
+          this.playerData.push({
+            role: "villager",
+            playerName: villager.name
+          });    
+        });
+      }
+      else{
+        this.playerData.push({
+          role: i,
+          playerName: roleObj[i].name
+        });
+      }
+    }
+  }
 
   ngOnInit() {
+    this.players.selectionChange.subscribe((s: MatSelectionListChange) => {
+
+      this.players.deselectAll();
+      s.option.selected = true;
+    });
   }
 
 }
