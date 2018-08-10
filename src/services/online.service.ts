17069c307e8fc7179daf26b9fafbc5574b1653c7
Otto Hu <ottohu101@gmail.com>
2018-08-09 23:30:46 -0700

    add version switch

--- src/services/online.service.ts
+++ src/services/online.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { UUID } from 'angular2-uuid';
 
 @Injectable({
   providedIn: 'root'
